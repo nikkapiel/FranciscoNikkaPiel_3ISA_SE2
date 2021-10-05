03aa8752ccb39e77bc8de9821a156e6764b641d2
Nikka Piel Francisco <nikkapiel.francisco.iics@ust.edu.ph>
2021-10-05 12:07:25 +0800

    app10.js

---
+++ node/app10.js
@@ -0,0 +1,14 @@
+app.get("/join-Covid", (req, res) => {
+    let sql = "SELECT Covid.id, Covid.fname, Covid.lname, Covid.age, Covid.symptoms" +
+    " FROM Covid" +
+    " INNER JOIN section" +
+    " ON Covid.id = number.id";
+    con.query(sql, (err, result) => {
+        if (!err) {
+            res.send(result);
+        } else {
+            res.send("failed to select Covid table");
+            throw err;
+        }
+    });
+});
